94d4f573913ec05d8d2db0b0ad492623bc111c39
Rufidatul Radium <60069688+Rufidatul726@users.noreply.github.com>
2023-03-04 17:55:21 +0600

    Add files via upload

---
+++ Project/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Homepage from './Dashboard/Homepage.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Homepage />
+      </header>
+    </div>
+  );
+}
+
+export default App;
