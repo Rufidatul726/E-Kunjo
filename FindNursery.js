14feab0eb0d6a1d9cecc7d3ed415ddf009b5601c
Rufidatul Radium <60069688+Rufidatul726@users.noreply.github.com>
2023-03-05 09:42:54 +0600

    Add files via upload

---
+++ FindNursery.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+function findNursery(){
+    return (
+        <div>
+            <div className="heading-container">
+                <h3>Find Nursery</h3>
+            </div>
+        </div>
+    );
+}
+
+export default findNursery;
---
+++ Homepage.js
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { ReactDOM } from 'react';
+import './Homepage.css';
+import Background from './images/Background.svg';
+
+
+function Homepage() {
+    return (
+      <div className="Homepage">
+        <header className="Homepage-header">
+          <h1>E-KUNJO</h1>
+          <h3>A GOOD HELP FOR PLANT-LOVERS</h3>
+        </header>
+        <body className="Homepage-body">
+          <div className='Homepage-Servdiv' id='Homepage-OurServices'>
+            <h1>Our Services</h1>
+          </div>
+          <div className='Homebody-Servdiv'>
+              <div><button className='App-Servbtn' onClick={}>Find Nursery</button></div>
+              <div><button className='App-Servbtn' >Get Plant Information</button></div>
+              <div><button className='App-Servbtn'>Get Fertilizer Amount</button></div>
+              <div><button className='App-Servbtn'>Detect Disease</button></div>
+          </div>
+        </body>
+      </div>
+    );
+  }
+  
+  export default Homepage;
